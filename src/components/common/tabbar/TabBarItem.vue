f70f4e5b14cafb684bbd8b22046ed8dab24ceefc
hanjinbo <15814073303@163.com>
2019-11-11 17:56:58 +0800

    第二次提交

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="tab-bar-item"
+    @click="itemClick"
+  >
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div>
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: ""
+  },
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  height: 49px;
+  font-size: 14px;
+  text-align: center;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
